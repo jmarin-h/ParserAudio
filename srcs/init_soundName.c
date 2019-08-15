76c9e0c1870cdfa78e3e37f46a9d768318ac8220
jmarin-h <jmarin-h@student.42.fr>
2019-08-15 20:53:06 +0200

    ...

--- srcs/init_soundName.c
+++ srcs/init_soundName.c
@@ -21,12 +21,15 @@ int		init_name(t_snd *snd)
 void	get_soundInfo(t_snd *snd, char *sound)
 {
 	int	i;
+	int	ok;
 
 	i = 0;
+	ok = 0;
 	while(i <= SOUNDS)
 	{
 		if(ft_strcmp(snd->effect[i].name, sound) == 0)
 		{
+			ok = 1;
 			ft_putendl(snd->effect[i].name);
 			ft_putendl(snd->effect[i].path);
 			ft_putstr("Overall size in bytes: ");
@@ -40,6 +43,8 @@ void	get_soundInfo(t_snd *snd, char *sound)
 			ft_putchar('\n');
 			printf("printf %f\n", snd->effect[i].in_seconds);
 		}
+		if(i == SOUNDS && ok == 0)
+			ft_putendl("Sound not found.");
 		i++;
 	}
 }
